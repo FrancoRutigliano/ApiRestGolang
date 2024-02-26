b78272b5170978f2df16156ac736441215ad300c
FrancoRutigliano <francorutigliano11@gmail.com>
2024-02-26 15:12:21 -0300

    fixing create endpoint and getById is working

--- controllers/books.go
+++ controllers/books.go
@@ -31,11 +31,17 @@ func CreateBook(c *gin.Context) {
 	book := models.Book{Title: input.Title, Author: input.Author}
 	models.DB.Create(&book)
 
-	c.JSON(http.StatusOK, gin.H{"created": book})
+	c.JSON(http.StatusCreated, gin.H{"created": book})
 }
 
 func GetBookById(c *gin.Context) {
+	var book models.Book
 
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
 }
 
 func ModifiedBook(c *gin.Context) {
