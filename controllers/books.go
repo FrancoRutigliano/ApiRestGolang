eb51114b80118dd8be81c4cf68c867b96ba052bf
FrancoRutigliano <francorutigliano11@gmail.com>
2024-02-27 10:28:23 -0300

    adding endpoint delete a record on the db  - working

--- controllers/books.go
+++ controllers/books.go
@@ -44,10 +44,37 @@ func GetBookById(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
 
-func ModifiedBook(c *gin.Context) {
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
 
+func ModifiedBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+	}
+
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	models.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
 }
 
 func DeleteBook(c *gin.Context) {
+	var book models.Book
 
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
